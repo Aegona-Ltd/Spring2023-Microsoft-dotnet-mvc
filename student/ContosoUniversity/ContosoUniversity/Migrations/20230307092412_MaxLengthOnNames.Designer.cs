2a69acd70ceaa3d0078f5b656688d45ec37bf504
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2023-03-07 16:25:05 +0700

    MVC

--- student/ContosoUniversity/ContosoUniversity/Migrations/20230307092412_MaxLengthOnNames.Designer.cs
+++ student/ContosoUniversity/ContosoUniversity/Migrations/20230307092412_MaxLengthOnNames.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    [Migration("20230307063454_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20230307092412_MaxLengthOnNames")]
+    partial class MaxLengthOnNames
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
