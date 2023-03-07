c2d16ec8ea547f4c419bf28e9957015247579e88
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2023-03-07 09:42:30 +0700

    intern
    C#

---
+++ AspMovie/AspMovie/Data/AspMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspMovie.Models;
+
+namespace AspMovie.Data
+{
+    public class AspMovieContext : DbContext
+    {
+        public AspMovieContext (DbContextOptions<AspMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MVC/ProductManagament/ProductManagament/Models/CategoryOfProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductManagament.Models
+{
+    public class CategoryOfProduct
+    {   
+            public int CategoryOfProductID { get; set; }
+
+            public string Description { get; set; }
+    }
+}
